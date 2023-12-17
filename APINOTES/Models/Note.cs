ce669d472c432a117a2e8f363423231f0f066f86
Osmel00 <osmel.fauregonzalez@dci.education>
2023-12-17 20:56:32 +0100

    commit 2

--- APINOTES/Models/Note.cs
+++ APINOTES/Models/Note.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace APINOTES.Models;
 
 public partial class Note
 {
     public int Id { get; set; }
-
+    
+    [Required]
     public string Tittel { get; set; } = null!;
-
+    
+    [Required]
     public string Description { get; set; } = null!;
 }
--- APINOTES/obj/Debug/net8.0/APINOTES.AssemblyInfo.cs
+++ APINOTES/obj/Debug/net8.0/APINOTES.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("6c25018b-5c73-4d0c-8efb-b12baf15561d")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("APINOTES")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eee7608a3d2faf67640804190d84b32c9ee33210")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b3f422b8ef8c3830b8b964df465ecaeea1cdbdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("APINOTES")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APINOTES")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
